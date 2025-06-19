e90773a917b893d51dbfcb12f092a551ae3b121f
gihary <chuckdbass@gmail.com>
2025-06-19 20:53:30 +0200

    Make parseTextFile async

--- src/parser/fileParser.js
+++ src/parser/fileParser.js
@@ -4,8 +4,8 @@ import fs from 'fs';
 /**
  * Read and return text from a file path.
  * @param {string} path - File system path
- * @returns {string} - File contents
- */
-export function parseTextFile(path) {
-  return fs.readFileSync(path, 'utf-8');
+ * @returns {Promise<string>} - File contents
+*/
+export async function parseTextFile(path) {
+  return await fs.promises.readFile(path, 'utf-8');
 }
